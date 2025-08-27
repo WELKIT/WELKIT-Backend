4cdde6175fa27c32b68d07f7a14c206b0fdf6df9
bum0w0 <kgy5988@naver.com>
2025-08-27 21:57:50 +0900

    #17 feat: 로그인 서비스 및 UserDetailsService 구현

---
+++ src/main/java/welkit_server/global/security/service/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package welkit_server.global.security.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import welkit_server.domain.user.entity.User;
+import welkit_server.domain.user.repository.UserRepository;
+import welkit_server.global.security.dto.CustomUserDetails;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(email + " not found"));
+
+        return new CustomUserDetails(user);
+    }
+
+}
