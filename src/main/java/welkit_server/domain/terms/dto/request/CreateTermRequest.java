3bc38b0d4fdfcdb6ad3f05ca1c852096aa6536c5
bum0w0 <kgy5988@naver.com>
2025-09-24 02:40:47 +0900

    #46 refactor: 용어/카테고리 DTO/Entity 필드 유효성 범위 수정

--- src/main/java/welkit_server/domain/terms/dto/request/CreateTermRequest.java
+++ src/main/java/welkit_server/domain/terms/dto/request/CreateTermRequest.java
@@ -1,6 +1,7 @@
 package welkit_server.domain.terms.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import welkit_server.domain.terms.entity.*;
 
@@ -11,13 +12,16 @@ import welkit_server.domain.terms.entity.*;
 public class CreateTermRequest {
 
     @NotBlank
+    @Size(min = 1, max = 50)
     private String name;
 
     @NotBlank
+    @Size(min = 1, max = 500)
     private String definition;
 
     private Long categoryId;
 
+    @Size(min = 1, max = 20)
     private String categoryName;
 
     public Term toEntity(TermCategory category) {
