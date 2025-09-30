48b54ea670f66659a72fbb0f44aaddf86868465e
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-09-30 11:34:33 +0900

    #62 fix: 용어 사전 응답 데이터에 카테고리 정보 포함 반환

--- src/main/java/welkit_server/domain/terms/repository/TermCategoryRepository.java
+++ src/main/java/welkit_server/domain/terms/repository/TermCategoryRepository.java
@@ -1,9 +1,12 @@
 package welkit_server.domain.terms.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import welkit_server.domain.terms.entity.TermCategory;
 import welkit_server.domain.user.entity.User;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +14,6 @@ public interface TermCategoryRepository extends JpaRepository<TermCategory, Long
 
     Optional<TermCategory> findByIdAndUser(Long categoryId, User user);
 
+    @Query("SELECT c FROM TermCategory c WHERE c.user = :user")
+    List<TermCategory> findAlLCategory(User user);
 }
