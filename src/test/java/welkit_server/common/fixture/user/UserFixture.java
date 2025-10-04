de4c20abbce7bbf5e70b2ddd68357d01da9ab985
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-10-04 19:43:10 +0900

    #72 test: 마이페이지 기능 잠금 설정 서비스 로직 테스트

--- src/test/java/welkit_server/common/fixture/user/UserFixture.java
+++ src/test/java/welkit_server/common/fixture/user/UserFixture.java
@@ -3,10 +3,26 @@ package welkit_server.common.fixture.user;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import welkit_server.domain.user.entity.User;
+import welkit_server.domain.user.model.EmailType;
 import welkit_server.domain.user.model.JobRole;
+import welkit_server.domain.user.model.UserType;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserFixture {
+
+    public static User user(Long id, String email, String password, JobRole jobRole) {
+        return User.builder()
+                .id(id)
+                .email(email)
+                .emailType(EmailType.COMPANY_EMAIL)
+                .password(password)
+                .jobRole(jobRole)
+                .isCompanyVerified(true)
+                .userType(UserType.USER)
+                .build();
+    }
+
+
 
     public static User createUser(String email, String password, JobRole jobRole) {
         return User.builder()
