59675a5f33580e0db1cacd405ba1921f1b65251b
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-10-11 17:46:12 +0900

    #77 test: 용어 등록 / 수정 서비스로직 테스트

--- src/test/java/welkit_server/service/terms/TermServiceTest.java
+++ src/test/java/welkit_server/service/terms/TermServiceTest.java
@@ -1,5 +1,6 @@
 package welkit_server.service.terms;
 
+import org.apache.coyote.BadRequestException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,7 +18,9 @@ import welkit_server.common.fixture.TermCategoryFixture;
 import welkit_server.common.fixture.TermFixture;
 import welkit_server.common.fixture.UserFixture;
 import welkit_server.domain.terms.dto.request.CreateTermRequest;
+import welkit_server.domain.terms.dto.request.EditTermRequest;
 import welkit_server.domain.terms.dto.response.CreateTermResponse;
+import welkit_server.domain.terms.dto.response.EditTermResponse;
 import welkit_server.domain.terms.dto.response.TermsResponse;
 import welkit_server.domain.terms.entity.Term;
 import welkit_server.domain.terms.entity.TermCategory;
@@ -29,10 +32,15 @@ import welkit_server.domain.user.entity.User;
 import welkit_server.domain.user.model.JobRole;
 import welkit_server.domain.user.model.UserType;
 import welkit_server.domain.user.service.UserService;
+import welkit_server.global.exception.message.ErrorMessage;
+import welkit_server.global.exception.model.NotFoundException;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 class TermServiceTest {
@@ -84,6 +92,7 @@ class TermServiceTest {
         );
 
     }
+
     @Test
     @DisplayName("용어 사전에서 전체 용어를 조회한다")
     void getAllTermsSuccess() {
@@ -107,6 +116,70 @@ class TermServiceTest {
         assertThat(response.getCategoryNames()).hasSize(1);
         assertThat(response.getCategoryNames().get(0).getCategoryName()).isEqualTo("카테고리");
 
+    }
+
+    @Test
+    @DisplayName("용어 등록 성공 테스트")
+    void createTermSuccess() {
+        //given
+        CreateTermRequest createTermRequest = CreateTermRequest.builder()
+                .name("용어2")
+                .definition("용어 등록 테스트")
+                .categoryId(1L)
+                .build();
+
+        given(userService.getAuthenticatedUser(authentication)).willReturn(user);
+        given(termCategoryService.findOrCreate(1L, null,authentication)).willReturn(termCategory);
+        given(termRepository.save(any(Term.class))).willAnswer(invocation -> invocation.getArgument(0));
+        //when
+        CreateTermResponse createTermResponse = termService.createTerm(createTermRequest, authentication);
+
+        assertThat(createTermResponse).isNotNull();
+        assertThat(createTermResponse.getName()).isEqualTo("용어2");
+        assertThat(createTermResponse.getDefinition()).isEqualTo("용어 등록 테스트");
+        assertThat(createTermResponse.getCategoryName()).isEqualTo("카테고리");
+    }
+
+    @Test
+    @DisplayName("용어 수정 성공 테스트")
+    void editTermSuccess() {
+        // given
+        EditTermRequest editTermRequest = EditTermRequest.builder()
+                .name("용어3")
+                .definition("용어 수정 테스트")
+                .categoryName("카테고리1")
+                .build();
+
+        given(userService.getAuthenticatedUser(authentication)).willReturn(user);
+        given(termRepository.findById(1L)).willReturn(Optional.of(term));
+        given(termCategoryService.findOrCreate(null, "카테고리1", authentication))
+                .willReturn(termCategory);
+
+        // when
+        EditTermResponse editTermResponse = termService.editTerm(1L, editTermRequest, authentication);
+
+        // then
+        assertThat(editTermResponse).isNotNull();
+        assertThat(editTermResponse.getName()).isEqualTo("용어3");
+        assertThat(editTermResponse.getDefinition()).isEqualTo("용어 수정 테스트");
+    }
+
+    @Test
+    @DisplayName("용어 수정 실패 테스트 - 존재하지 않는 용어를 수정할시 예외 발생 ")
+    void editTermFail() {
+        //given
+        EditTermRequest editTermRequest = EditTermRequest.builder()
+                .name("용어3")
+                .definition("용어 수정 테스트")
+                .categoryName("카테고리1")
+                .build();
+        given(userService.getAuthenticatedUser(authentication)).willReturn(user);
+        given(termRepository.findById(2L)).willReturn(Optional.empty());
+
+        //when then
+        assertThatThrownBy(() -> termService.editTerm(2L, editTermRequest, authentication))
+                .isInstanceOf(NotFoundException.class)
+                .hasMessageContaining(ErrorMessage.TERM_NOT_FOUND.getMessage());
     }
 
 
